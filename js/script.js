7a8a09c4f9f3a89bc18d71f10d79b3fffe7d87d6
ProgrammingHero1 <rabbyrj018@gmail.com>
2025-05-13 08:59:50 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed =new Typed(".text",{
+    strings:["Font-end developer", "Electronics Engineer","History Explorer"],
+    typeSpeed:100,
+    backspeed:100,
+    backDelay:1000,
+    loop:true
+})
